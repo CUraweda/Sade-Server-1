f360af7f9111d6da61f93b9645f53f15b4eb104a
Diaz-adrianz <diazadrianz191205@gmail.com>
2024-07-04 15:50:40 +0700

    refactor(table): add billing cycle
    Signed-off-by: Diaz-adrianz <diazadrianz191205@gmail.com>

--- src/route/paymentPostRoute.js
+++ src/route/paymentPostRoute.js
@@ -10,22 +10,22 @@ const paymentPostValidator = new PaymentPostValidator();
 
 router.post(
   "/create",
-  auth([1]),
+  auth([1, 2]),
   paymentPostValidator.paymentPostCreateUpdateValidator,
   paymentPostController.create
 );
 
 router.put(
   "/update/:id",
-  auth([1]),
+  auth([1, 2]),
   paymentPostValidator.paymentPostCreateUpdateValidator,
   paymentPostController.update
 );
 
-router.get("/show/:id", auth([1]), paymentPostController.show);
+router.get("/show/:id", auth([1, 2]), paymentPostController.show);
 
-router.get("/", auth([1]), paymentPostController.showAll);
+router.get("/", auth([1, 2]), paymentPostController.showAll);
 
-router.delete("/delete/:id", auth([1]), paymentPostController.delete);
+router.delete("/delete/:id", auth([1, 2]), paymentPostController.delete);
 
 module.exports = router;

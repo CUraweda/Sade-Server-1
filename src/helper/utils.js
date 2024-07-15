04d2a5c2276996c848ae7ed8687649cc190adf4e
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-07-15 15:57:38 +0700

    fix: filter where lower than now

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -1,21 +1,25 @@
-const formatDateForSQL = (date) => {
-	const pad = (num) => (num < 10 ? '0' + num : num);
-
-	return (
-		date.getFullYear() +
-		'-' +
-		pad(date.getMonth() + 1) +
-		'-' +
-		pad(date.getDate()) +
-		' ' +
+const formatDateForSQL = (date, withTime = false) => {
+	const pad = (num) => (num < 10 ? "0" + num : num);
+  
+	let result =
+	  date.getFullYear() +
+	  "-" +
+	  pad(date.getMonth() + 1) +
+	  "-" +
+	  pad(date.getDate());
+  
+	if (withTime)
+	  result +=
+		" " +
 		pad(date.getHours()) +
-		':' +
+		":" +
 		pad(date.getMinutes()) +
-		':' +
-		pad(date.getSeconds())
-	);
+		":" +
+		pad(date.getSeconds());
+  
+	return result;
 };
 
 module.exports = {
-	formatDateForSQL
-}
+	formatDateForSQL,
+};

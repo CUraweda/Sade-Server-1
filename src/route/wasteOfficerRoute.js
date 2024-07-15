69baf2bdfa2027965328a7285966983bf3fed7dc
beilmouz <nutbill30@gmail>
2024-07-15 11:19:53 +0700

    feat: add auth validation for petugas dan admin timbangan

--- src/route/wasteOfficerRoute.js
+++ src/route/wasteOfficerRoute.js
@@ -10,23 +10,23 @@ const wasteOfficerValidator = new WasteOfficerValidator();
 
 router.post(
   "/create",
-  auth([1]),
+  auth([1, 9, 10]),
   wasteOfficerValidator.wasteOfficerCreateUpdateValidator,
   wasteOfficerController.create
 );
 
 router.put(
   "/update/:id",
-  auth([1]),
+  auth([1, 9, 10]),
   wasteOfficerValidator.wasteOfficerCreateUpdateValidator,
   wasteOfficerController.update
 );
 
-router.get("/show/:id", auth([1]), wasteOfficerController.show);
+router.get("/show/:id", auth([1, 9, 10]), wasteOfficerController.show);
 
-router.get("/get-by-date/:date", auth([1]), wasteOfficerController.showByDate);
-router.get("/", auth([1]), wasteOfficerController.showAll);
+router.get("/get-by-date/:date", auth([1, 9, 10]), wasteOfficerController.showByDate);
+router.get("/", auth([1, 9, 10]), wasteOfficerController.showAll);
 
-router.delete("/delete/:id", auth([1]), wasteOfficerController.delete);
+router.delete("/delete/:id", auth([1, 9, 10]), wasteOfficerController.delete);
 
 module.exports = router;

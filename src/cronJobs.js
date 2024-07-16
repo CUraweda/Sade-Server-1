6ed0ee90c994aca442575e6cebfc95ce2e1fb051
Diaz-Adrianz <diazadrianz191205@gmail.com>
2024-07-16 07:37:57 +0700

    fix: cron intertval

--- src/cronJobs.js
+++ src/cronJobs.js
@@ -2,4 +2,4 @@ const cron = require('node-cron');
 const sycnReportAccessByPayment = require('./service/cron/SyncReportAccessByPaymentBills');
 
 // Sync report access by bills
-cron.schedule('*/30 * * * * *', sycnReportAccessByPayment);
+cron.schedule('*/30 * * * *', sycnReportAccessByPayment);

851207e8b051570863d17f5d37cb1412488ecbc8
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-07-18 11:37:22 +0700

    Change identifier subject from id to code

--- src/controllers/NumberReportController.js
+++ src/controllers/NumberReportController.js
@@ -135,7 +135,7 @@ class NumberReportController {
   exportByStudentId = async (req, res) => {
     try {
       var id = req.params.id;
-      const semester = req.query.semester;
+      const semester = req.query.semester || 1;
 
       const resData = await this.numberReportService.exportReportByStudentId(
         id,

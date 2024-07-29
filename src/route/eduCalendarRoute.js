63f1f3c33bdba4ef8835898808fb8c8e82f91016
beilmouz <nutbill30@gmail>
2024-07-29 15:52:18 +0700

    delete teacher from calendar

--- src/route/eduCalendarRoute.js
+++ src/route/eduCalendarRoute.js
@@ -10,14 +10,14 @@ const eduCalendarValidator = new EduCalendarValidator();
 
 router.post(
   "/create",
-  auth([1, 3, 4, 6]),
+  auth([1, 3, 4]),
   eduCalendarValidator.eduCalendarCreateUpdateValidator,
   eduCalendarController.create
 );
 
 router.put(
   "/update/:id",
-  auth([1, 3, 4, 6]),
+  auth([1, 3, 4]),
   eduCalendarValidator.eduCalendarCreateUpdateValidator,
   eduCalendarController.update
 );
@@ -38,7 +38,7 @@ router.get("/", auth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), eduCalendarController.sho
 
 router.delete(
   "/delete/:id",
-  auth([1, 3, 4, 6]),
+  auth([1, 3, 4]),
   eduCalendarController.delete
 );
 

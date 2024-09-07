9d670b9edef83594cc77d46818b149c1597525f0
beilmouz <nutbill30@gmail>
2024-09-07 13:42:44 +0700

    fix: generate portofolio comments

--- src/service/PortofolioReportService.js
+++ src/service/PortofolioReportService.js
@@ -187,6 +187,7 @@ class PortofolioReportService {
   };
 
   mergePDFs = async (pdf1, pdf2) => {
+    console.log("WHERTE TF IT IS FROM",pdf1, pdf2)
     try {
       const pdfBytes1 = await fs.readFile(pdf1);
       const pdfBytes2 = await fs.readFile(pdf2);

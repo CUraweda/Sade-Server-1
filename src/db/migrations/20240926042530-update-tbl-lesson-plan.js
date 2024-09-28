68ee6cc535d72ea7a132b19b2fad92ce136ff152
hafizhproject45 <hafizh.project45@gmail.com>
2024-09-28 11:31:33 +0700

    fix: remove column lesson update lesson plan

--- src/db/migrations/20240926042530-update-tbl-lesson-plan.js
+++ src/db/migrations/20240926042530-update-tbl-lesson-plan.js
@@ -2,8 +2,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('tbl_lesson_plan', 'class')
-
     await queryInterface.addColumn('tbl_lesson_plan', 'class_id', {
       type: Sequelize.INTEGER,
       allowNull: false,
@@ -16,8 +14,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('tbl_lesson_plan', 'class_id')
-
     await queryInterface.addColumn('tbl_lesson_plan', 'class', { type: Sequelize.STRING });
   }
 };

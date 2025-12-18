cce7d6f27b5c539c9e2d31497c68f44a45f2909d
Diaz-Adrianz <diazz.developer@gmail.com>
2025-12-18 18:35:48 +0700

    chore: update logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,41 +1,36 @@
-const winston = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
+const winston = require("winston")
+const DailyRotateFile = require("winston-daily-rotate-file")
 const config = require('./config');
 
-const enumerateErrorFormat = winston.format((info) => {
-    if (info.message instanceof Error) {
-        info.message = {
-            message: info.message.message,
-            stack: info.message.stack,
-            ...info.message,
-        };
-    }
 
-    if (info instanceof Error) {
-        return { message: info.message, stack: info.stack, ...info };
-    }
+const { combine, timestamp, printf, colorize, errors } = winston.format;
 
-    return info;
-});
-const transport = new DailyRotateFile({
-    filename: config.logConfig.logFolder + config.logConfig.logFile,
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '3',
-    prepend: true,
-});
-transport.on('rotate', (oldFilename, newFilename) => {
-    // call function like upload to s3 or on cloud
-});
+const logFormat = combine(
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    errors({ stack: true }),
+    colorize(),
+    printf(({ level, message, timestamp, stack }) => {
+        return stack
+            ? `${timestamp} ${level}: ${message} - ${stack}`
+            : `${timestamp} ${level}: ${message}`;
+    })
+);
 
 const logger = winston.createLogger({
-    format: winston.format.combine(enumerateErrorFormat(), winston.format.json()),
+    format: logFormat,
     transports: [
-        transport,
         new winston.transports.Console({
-            level: 'info',
+            format: winston.format.combine(winston.format.colorize(), logFormat),
+        }),
+        new DailyRotateFile({
+            filename: config.logConfig.logFolder + config.logConfig.logFile,
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '3',
+            prepend: true,
         }),
     ],
 });
+
 module.exports = logger;
